dc6ec41685d65c1662b5bd0f4ec8f23b8bd6910d
Walaryne <hazardoustheradioactive@gmail.com>
2021-03-28 02:05:40 -0400

    Added autologout

---
+++ MinecraftClient/ChatBots/AutoLogout.cs
@@ -0,0 +1,28 @@
+﻿namespace MinecraftClient.ChatBots
+{
+    /// <summary>
+    /// Automatically logs you out when your HP reaches a certain threshold.
+    /// </summary>
+    public class AutoLogout : ChatBot
+    {
+        int hpThreshold;
+
+        /// <summary>
+        /// Automatically logs you out when your HP reaches a certain threshold.
+        /// </summary>
+        /// <param name="hpThreshold">Threshold for HP to cause an automatic logout.</param>
+        public AutoLogout(int hpThreshold)
+        {
+            this.hpThreshold = hpThreshold;
+        }
+
+        public override void OnHealthUpdate(float health, int food)
+        {
+            if(health < hpThreshold && !((int) health == 0))
+            {
+                LogToConsole("Player HP below threshold, exiting MCC!");
+                DisconnectAndExit();
+            }
+        }
+    }
+}
